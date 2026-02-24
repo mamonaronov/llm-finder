6391993eb09ac56db0d91184944d02a51984db1c
mamonaronov <mynewab613@gmail.com>
2026-02-24 13:03:03 +0300

    минимальный рабочий вариант с заглушкой
    теперь сервис поднимается с заглушкой и выдает написанные в заглушке результаты

--- python/search_service.py
+++ python/search_service.py
@@ -1,5 +1,7 @@
 from fastapi import FastAPI, HTTPException
+from pydantic import BaseModel
 import os
+from typing import List, Optional
 
 app = FastAPI()
 
@@ -8,23 +10,35 @@ COLLECTION_NAME = os.getenv("COLLECTION_NAME", "my_docs")
 
 
 
-@app.post("/search")
-async def search():
+# Модель запроса
+class SearchRequest(BaseModel):
+    query: str
+    top_k: Optional[int] = 5
+
+# Модель ответа
+class SearchResult(BaseModel):
+    id: str
+    text: str
+    source: str
+    score: float
+
+
+
+@app.post("/search", response_model=List[SearchResult])
+async def search(request: SearchRequest):
     try:
-
-
-
 
 
         ## пока что тут заглушка
 
+        results = []
+        results.append(SearchResult(
+            id='121',
+            text='пример текста',
+            source='example_link_of_dockument',
+            score=0.613))
 
 
-
-
-
-
-        results = 'ссылка на статью'
         return results
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
