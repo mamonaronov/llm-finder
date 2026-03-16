f5a287acb03dee68f565f14c4446f928de8a6d46
mamonaronov <mynewab613@gmail.com>
2026-03-16 14:42:48 +0300

    Update search_service.py

--- python/search_service.py
+++ python/search_service.py
@@ -34,10 +34,14 @@ async def search(request: SearchRequest):
         results = []
         results.append(SearchResult(
             id='121',
+            text='пример текста (кстати запрос 1 был таким: "' + request.query + '")',
+            source='example_link_of_dockument',
+            score=0.613))
+        results.append(SearchResult(
+            id='121',
             text='пример текста (кстати запрос был таким: "' + request.query + '")',
             source='example_link_of_dockument',
             score=0.613))
-
 
         return results
     except Exception as e:
