f5f53e9708a27539e0373cad9ca5f38f5eebdd21
Gumenik <intuitsi@ya.ru>
2026-05-12 23:37:35 +0300

    New Indexing script for new HDF5

--- load_data_to_qdrant_volume/index_docs.py
+++ load_data_to_qdrant_volume/index_docs.py
@@ -37,39 +37,24 @@ print([c for c in collections])
 
 # загрузка h5 файла
 with h5py.File("habr_embeddings.h5", 'r') as f: #Файловы тип данных
-    keys = f['keys'][:] #Индекс [:] означает что нужно выбрать все значения
+    keys_struct = f['keys'][:] #Индекс [:] означает что нужно выбрать все значения
     tensors = f['tensors'][:] #тип - numpy array
-vectors = [tensor for tensor in tensors]
-payloads = [{'text':key.decode('utf-8')} for key in keys]
-
-
-# старый способ загрузки векторов в qdrant
-"""points = []
-for it, vec in zip(items, vectors):
-    payload = dict(it)
-    pid = payload.pop("id", "link")
-    points.append(PointStruct(id=pid, vector=vec.tolist(), payload=payload))
-
-
-client.upsert(collection_name="collection_name", points=points)
-"""
-
-
 
 # Генератор точек – не хранит все в памяти
-def point_generator(payloads, vectors):
-    i=0
-    for pl, vec in zip(payloads, vectors):
-        payload = dict(pl)
-        # можно сразу возвращать PointStruct, а можно кортеж – upload_points сам поймёт
-        yield PointStruct(id = i, vector=vec.tolist(), payload=payload)
-        i+=1
+def point_generator(keys_struct, tensors):
+    for i, (row, vec) in enumerate(zip(keys_struct, tensors)):
+        point_id = row['id']
+        url = row['url']
+        text = row['text_markdown']
+        payload = {"text": text, "link": url}
+        yield PointStruct(id=point_id, vectors=vec.tolist(), payload=payload)
+
 
 
 # Загрузка батчами по умолчанию 64 точки
 client.upload_points(
     collection_name=collection_name,
-    points=point_generator(payloads, vectors),
+    points=point_generator(keys_struct, tensors),
     batch_size=5,  # можно настроить под свои задачи
     wait=True,  # дождаться подтверждения записи
 )
