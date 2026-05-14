c51ae09455caf34916f824b70310681bc935fa50
Gumenik <intuitsi@ya.ru>
2026-05-14 17:10:48 +0300

    Correction for indexing method

--- load_data_to_qdrant_volume/index_docs.py
+++ load_data_to_qdrant_volume/index_docs.py
@@ -35,22 +35,21 @@ collections = client.get_collections()
 print([c for c in collections])
 
 
-# загрузка h5 файла
-with h5py.File("habr_embeddings.h5", 'r') as f: #Файловы тип данных
-    keys_struct = f['keys'][:] #Индекс [:] означает что нужно выбрать все значения
-    tensors = f['tensors'][:] #тип - numpy array
+
+with h5py.File("habr_embeddings.h5", 'r') as f:
+    keys_struct = f['keys'][:]
+    tensors = f['tensors'][:]
 
 def decode_if_bytes(val):
     return val.decode('utf-8') if isinstance(val, bytes) else val
 
-# Генератор точек – не хранит все в памяти
 def point_generator(keys_struct, tensors):
     for i, (row, vec) in enumerate(zip(keys_struct, tensors)):
         point_id = uuid.uuid4()
         url = decode_if_bytes(row['url'])
         text = decode_if_bytes(row['text_markdown'])
         payload = {"text": text, "link": url}
-        yield PointStruct(id=point_id, vectors=vec.tolist(), payload=payload)
+        yield PointStruct(id=point_id, vector=vec.tolist(), payload=payload)
 
 
 
